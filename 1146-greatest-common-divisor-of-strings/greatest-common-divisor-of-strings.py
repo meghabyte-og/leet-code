18dbeade1c2044b845e1287a5d97f7c4ea444690
Megha <kbmegg@gmail.com>
2025-07-19 18:35:57 -0700

    Time: 0 ms (100.00%) | Memory: 18 MB (26.81%) - LeetSync

--- 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.py
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.py
@@ -1,7 +1,7 @@
 class Solution:
     def gcdOfStrings(self, str1: str, str2: str) -> str:
         g= math.gcd(len(str1),len(str2))
-        if(str1== str1[0:g]*(len(str1)//g) and str2== str1[0:g]*(len(str2)//g)):
+        if(str1==str1[0:g]*(len(str1)//g) and str2== str1[0:g]*(len(str2)//g)):
             return(str1[0:g])
         else: return ""
         
