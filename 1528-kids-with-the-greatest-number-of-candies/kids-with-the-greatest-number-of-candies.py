10ee8f1cd48f6740c510c1379ae4c12b87126b9b
Megha <kbmegg@gmail.com>
2025-07-21 13:33:24 -0700

    Time: 2 ms (17.66%) | Memory: 17.7 MB (84.73%) - LeetSync

--- 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.py
+++ 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.py
@@ -1,6 +1,4 @@
 class Solution:
     def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
-        greatest=[]
-        m=max(candies) #we only need to check if the max is greater than the max of existing array
-        return[extraCandies+i>=m for i in candies]
+        return[extraCandies+i>=max(candies) for i in candies]
         
