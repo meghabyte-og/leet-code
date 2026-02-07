4d7e3867aefc6710b43fe520f43b588479341ed1
Megha <kbmegg@gmail.com>
2026-02-06 19:28:38 -0700

    Time: 0 ms (100.00%) | Memory: 19.3 MB (38.86%) - LeetSync

--- 541-reverse-string-ii/reverse-string-ii.py
+++ 541-reverse-string-ii/reverse-string-ii.py
@@ -1,8 +1,7 @@
 class Solution:
     def reverseStr(self, s: str, k: int) -> str:
         n=len(s)
-        if n<k:
-            return s[::-1]
+        
         s=list(s)
         
         for i in range(0,n,2*k):
