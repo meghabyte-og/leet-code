674f49875f22fa8f63a8c5699d517323706f4e93
Megha <kbmegg@gmail.com>
2026-06-02 11:11:36 -0700

    Time: 937 ms (13.85%) | Memory: 12.5 MB (17.91%) - LeetSync

--- 69-sqrtx/sqrtx.py
+++ 69-sqrtx/sqrtx.py
@@ -1,10 +1,17 @@
-class Solution:
-    def mySqrt(self, x: int) -> int:
-        if x==0:
+class Solution(object):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        if x == 0:
             return 0
-        if x<=2:
+        if x <= 2:
             return 1
-
-        for i in range(x):  
+        
+        for i in xrange(x):
             if i*i > x:
                 return i-1
+        
+         
+        
