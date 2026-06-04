96b0536a7ce4a93a085da4eb6af29aba421c14ef
Megha <kbmegg@gmail.com>
2026-06-04 16:33:47 -0700

    Time: 231 ms (92.94%) | Memory: 19.3 MB (68.40%) - LeetSync

--- 4057-total-waviness-of-numbers-in-range-i/total-waviness-of-numbers-in-range-i.py
+++ 4057-total-waviness-of-numbers-in-range-i/total-waviness-of-numbers-in-range-i.py
@@ -3,14 +3,12 @@ class Solution(object):
         count = 0
         for i in range(num1, num2+1):
             i = str(i)
-            print(i)
             for j in range(1, len(i)-1):
                 
                 prev = i[j-1]
                 curr = i[j]
                 nex = i[j+1]
                 if prev < curr > nex or prev > curr < nex:
-                    print('True')
                     count += 1
         return count
 
