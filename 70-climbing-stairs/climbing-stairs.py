00ed736852be265feadcb619f6c672707f3c4851
Megha <kbmegg@gmail.com>
2026-07-07 12:50:36 -0700

    Time: 0 ms (100.00%) | Memory: 19.4 MB (16.48%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -1,9 +1,14 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
+        # F(n) = F(n-1) + F(n-2)
+        # F(1) = 1
+        # F(2) = 2
         if n <= 2:
             return n
-        a = 1
-        b = 2
-        for i in range(3,n+1):
-            a, b = b, a+b
-        return b
+        f1 = 1
+        f2 = 2
+        f3 = 2
+        for i in range(3, n+1):
+            f3 = f1 + f2
+            f1, f2 = f2, f3
+        return f3
