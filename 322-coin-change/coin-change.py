c1877fac2825acf4bde0312772d5dd03e751ec97
Megha <kbmegg@gmail.com>
2026-07-28 12:06:19 -0700

    Time: 1459 ms (5.02%) | Memory: 24.5 MB (21.04%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -1,6 +1,23 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         memo = [[-1]*(len(coins)+1) for _ in range(amount+1)]
+
+        for i in range(amount+1):
+            memo[i][0] = float('inf')
+        for j in range(len(coins)+1):
+            memo[0][j] = 0
+
+        for i in range(1, amount+1):
+            for j in range(1, len(coins)+1):
+                if i >= coins[j-1]:
+                    memo[i][j] = min(1 + memo[i-coins[j-1]][j-1], 1 + memo[i-coins[j-1]][j], memo[i][j-1])
+                else:
+                    memo[i][j] = memo[i][j-1]
+        return memo[-1][-1] if memo[-1][-1] != float('inf') else -1
+                
+                
+
+        """
         def dp(amount, i):
             if amount == 0 :
                 return 0
@@ -23,3 +40,4 @@ class Solution:
         
         result = dp(amount, 0)
         return result if result != float('inf') else -1
+        """
