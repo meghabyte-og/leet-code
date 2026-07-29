71746e820d3be1c34227d3fb0a069b2b6c73c423
Megha <kbmegg@gmail.com>
2026-07-28 19:30:51 -0700

    Time: 469 ms (44.12%) | Memory: 52.9 MB (43.23%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.py
+++ 518-coin-change-ii/coin-change-ii.py
@@ -1,8 +1,25 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
+        
+        dp = [[-1]*(len(coins)+1) for _ in range(amount+1)]
 
-        memo = [[-1]*(len(coins)+1) for _ in range(amount+1)]
+        #dp[amount][i]
 
+        for i in range(len(dp[0])):
+            dp[0][i] = 1
+        
+        for i in range(1, len(dp)):
+            dp[i][len(coins)] = 0
+        
+        for i in range(len(coins)-1, -1, -1):
+            for a in range(1, amount+1):
+                if coins[i] <= a:
+                    dp[a][i] = dp[a - coins[i]][i] + dp[a][i+1]
+                else:
+                    dp[a][i] = dp[a][i+1]
+        return(dp[-1][0])
+
+        """
         def dp(amount, i):
 
             if amount == 0:
@@ -26,4 +43,4 @@ class Solution:
             
             
 
-            
+        """
