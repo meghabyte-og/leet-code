d1287b22e712fa7410f021b2e685d0893202a594
Megha <kbmegg@gmail.com>
2026-08-02 10:52:22 -0700

    Time: 0 ms (100.00%) | Memory: 19.2 MB (75.06%) - LeetSync

--- 909-stone-game/stone-game.py
+++ 909-stone-game/stone-game.py
@@ -1,16 +1,6 @@
 class Solution:
     def stoneGame(self, nums: List[int]) -> bool:
-        if len(nums) == 1:
-            return True
-        dp = [[0]* (len(nums)+1) for _ in range(len(nums)+1)]
-        def recurse(start, end):
-            if start > end:
-                return 0
-            if dp[start][end] != 0:
-                return dp[start][end]
-            dp[start][end] = max(nums[start] - recurse(start+1, end), nums[end] - recurse(start, end-1))  
-            return dp[start][end]            
-        return True if recurse(0, len(nums)-1)>=0 else False
+        return True
 
                 
             
