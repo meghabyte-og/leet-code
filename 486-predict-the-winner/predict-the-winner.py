b744887e10aaab286691fc2412a5e02b4e5684af
Megha <kbmegg@gmail.com>
2026-08-02 13:01:40 -0700

    Time: 2844 ms (5.01%) | Memory: 19.3 MB (78.41%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.py
+++ 486-predict-the-winner/predict-the-winner.py
@@ -1,20 +1,12 @@
 class Solution:
     def predictTheWinner(self, nums: List[int]) -> bool:
-        if len(nums) == 1:
-            return True
         def recurse(start, end):
-        
             if start > end:
                 return 0
-
-            return max(nums[start] - recurse(start+1, end), nums[end] - recurse(start, end-1))
-            
-            #  - min(nums[start] + recurse(start+1, end), nums[end] + recurse(start, end-1))
-                
-            
-        
-        
-        return True if recurse(0, len(nums)-1)>=0 else False
-
-                
+            return max(
+                nums[start] - recurse(start+1, end),
+                nums[end] - recurse(start, end-1)
+            )
+        dp = recurse(0, len(nums)-1)
+        return True if dp >=0 else False
             
