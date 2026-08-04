2257d4b44971c919b96204daab32b872964c1d1e
Megha <kbmegg@gmail.com>
2026-08-04 14:15:36 -0700

    Time: 0 ms (100.00%) | Memory: 19.5 MB (65.58%) - LeetSync

--- 46-permutations/permutations.py
+++ 46-permutations/permutations.py
@@ -1,17 +1,13 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        def recurse(l):
-            if len(l)==0:
-                return [[]]
-            final=[]
-            for i in range(len(l)):
-                print(l[:i]+l[i+1:])
-                for j in recurse(l[:i]+l[i+1:]):
-                    final.append([l[i]]+j)
-            print(final)
-            return final
-        return(recurse(nums))
-
-
-                       
-                
+        result = []
+        def backtrack(curr, rem) -> None:
+            if len(rem) == 0:
+                result.append(curr)
+                return
+            for i in range(len(rem)):
+                backtrack(curr+[rem[i]], rem[:i]+rem[i+1:])
+            return
+        backtrack([], nums)
+        return result
+            
