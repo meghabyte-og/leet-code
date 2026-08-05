9cc90998ef59483df6d623d0a60009d6ae6d8b71
Megha <kbmegg@gmail.com>
2026-08-05 13:32:59 -0700

    Time: 7 ms (43.80%) | Memory: 21.8 MB (6.15%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -5,33 +5,35 @@ class Solution:
             adj[i] = []
         for i,j in pre:
             adj[i].append(j)
+
         visited = set()
-        curr_path = set()
+        currpath = set()
+        
         def dfs(node):
             visited.add(node)
-            curr_path.add(node)
+            currpath.add(node)
+
             for neighbor in adj[node]:
                 if neighbor not in visited:
-                    visited.add(neighbor)
-                    curr_path.add(neighbor)
                     ans = dfs(neighbor)
                     if ans == False:
                         return False
-                    curr_path.remove(neighbor)
-                elif neighbor in visited and neighbor in curr_path:
-                    return False
+                    currpath.remove(neighbor)
+                else:
+                    if neighbor in currpath:
+                        return False
             return True
         
-        for i in range(numCourses):
-            if i not in visited:
-                curr_path.add(i)
-                visited.add(i)
-                ans = dfs(i)
-            if not ans:
-                return False
-            if i in curr_path:
-                curr_path.remove(i)
+        for c in range(numCourses):
+            if c not in visited:
+                ans = dfs(c)
+                if ans == False:
+                    return False
+                if c in currpath:
+                    currpath.remove(c)
         return True
+            
+                    
         
 
                 
