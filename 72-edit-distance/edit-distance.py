9fe6a453b26bef5a28aacf152879c2c30c291cec
Megha <kbmegg@gmail.com>
2026-08-05 15:47:59 -0700

    Time: 43 ms (71.93%) | Memory: 22.6 MB (64.87%) - LeetSync

--- 72-edit-distance/edit-distance.py
+++ 72-edit-distance/edit-distance.py
@@ -2,24 +2,22 @@ class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
         m = len(word1)
         n = len(word2)
+        if m == 0:
+            return n
+        if n == 0:
+            return m
         dp = [[0]*(n+1) for _ in range(m+1)]
-        def recurse(i, j):
-            if i == 0:
-                return j
-            if j == 0:
-                return i
 
-            if dp[i][j] != 0:
-                return dp[i][j]
+        for i in range(m + 1):
+            dp[i][0] = i
+        for j in range(n + 1):
+            dp[0][j] = j
+        
+        for i in range(1, m+1):
+            for j in range(1, n+1):
+                if word1[i-1] == word2[j-1]:
+                    dp[i][j] = dp[i-1][j-1]
+                else:
+                    dp[i][j] = 1 + min(dp[i-1][j], dp[i][j-1], dp[i-1][j-1])
 
-            if word1[i-1] == word2[j-1]:
-                dp[i][j] = recurse(i-1, j-1)
-            
-            else:
-                dp[i][j] = 1 + min(
-                    recurse(i, j-1),
-                    recurse(i-1, j),
-                    recurse(i-1, j-1)
-                )
-            return dp[i][j]
-        return recurse(m,n)
+        return dp[-1][-1] 
