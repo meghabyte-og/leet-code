089e173dfde2dd7cecee4786aba984fc8466eef9
Megha <kbmegg@gmail.com>
2026-08-07 11:52:05 -0700

    Time: 179 ms (25.09%) | Memory: 49.2 MB (59.41%) - LeetSync

--- 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/reorder-routes-to-make-all-paths-lead-to-the-city-zero.py
+++ 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/reorder-routes-to-make-all-paths-lead-to-the-city-zero.py
@@ -1,34 +1,27 @@
 class Solution:
     def minReorder(self, n: int, connections: List[List[int]]) -> int:
-        adj = dict()
-        edge = dict()
+        undirected = dict()
+        directed = dict()
         for i in range(n):
-            adj[i] = []
-            edge[i] = []
-        for i,j in connections:
-            adj[i].append(j)
-            edge[i].append(j)
-            edge[j].append(i)
+            undirected[i] = []
+            directed[i] = []
+        for i, j in connections:
+            undirected[i].append(j)
+            undirected[j].append(i)
+            directed[i].append(j)
         
+        count = 0
         q = deque()
+        q.append(0)
         visited = set()
         visited.add(0)
-        q.append(0)
-
-        count = 0
-        # bfs = []
         while q:
             curr = q.popleft()
-            for neighbor in edge[curr]:
-                if neighbor not in visited:
-                    visited.add(neighbor)
-                    q.append(neighbor)
-                    # bfs.append(neighbor)
-                    if curr not in adj[neighbor]:
+            for i in undirected[curr]:
+                if i not in visited:
+                    visited.add(i)
+                    q.append(i)
+                    if i in directed[curr]:
                         count += 1
-        # print(bfs)
+       
         return count
-
-
-            
-        
