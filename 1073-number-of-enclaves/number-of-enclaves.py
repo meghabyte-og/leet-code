60cc91d3c946184a99366c6bd8b5444884f4a258
Megha <kbmegg@gmail.com>
2026-08-07 19:41:05 -0700

    Time: 111 ms (21.83%) | Memory: 40.5 MB (21.64%) - LeetSync

---
+++ 1073-number-of-enclaves/number-of-enclaves.py
@@ -0,0 +1,48 @@
+class Solution:
+    def numEnclaves(self, grid: List[List[int]]) -> int:
+        #non corner islands
+
+        m = len(grid)
+        n = len(grid[0])
+        directions = [(0,1), (1,0), (-1,0), (0,-1)]
+        visited = [[0]*n for _ in range(m)]
+
+        def isValid(i,j):
+            if i<0 or j<0 or i>=m or j>=n:
+                return False
+            if visited[i][j]:
+                return False
+            if grid[i][j] == 0:
+                return False
+            return True
+
+
+        def dfs(i,j):
+            if not isValid(i,j):
+                return
+
+            visited[i][j] = 1
+
+            for di, dj in directions:
+                ni = i + di
+                nj = j + dj
+                if not isValid(ni, nj):
+                    continue
+                dfs(ni, nj)
+        
+        
+        noncorner = 0
+        for i in range(m):
+            for j in range(n):
+
+                if i == 0 or j == 0 or i == m-1 or j == n-1:
+                    if grid[i][j] == 1 and not visited[i][j]:
+                        dfs(i,j)
+                
+        for i in range(1, m-1):
+            for j in range(1, n-1):
+                if grid[i][j] == 1 and not visited[i][j]:
+                    noncorner += 1
+        
+        return noncorner
+                    
