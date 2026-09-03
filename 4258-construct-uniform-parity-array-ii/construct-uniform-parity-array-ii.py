4763fec33d7300f5d37999b6750406cfe2cc18ed
Megha <kbmegg@gmail.com>
2026-09-03 08:32:28 -0700

    Time: 10 ms (92.07%) | Memory: 35.3 MB (73.17%) - LeetSync

--- 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.py
+++ 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.py
@@ -2,8 +2,8 @@ class Solution:
     def uniformArray(self, nums1: list[int]) -> bool:
         if len(nums1) <= 1:
             return True
-        nums1.sort()
-        smallest = nums1[0]
+
+        smallest = min(nums1)
         
         if smallest % 2 != 0:
             return True
