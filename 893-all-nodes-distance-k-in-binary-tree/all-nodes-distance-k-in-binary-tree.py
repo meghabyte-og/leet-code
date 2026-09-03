2ede4ff9453bd82e04ffaded23958bebe4f3b111
Megha <kbmegg@gmail.com>
2026-09-03 10:04:44 -0700

    Time: 53 ms (28.69%) | Memory: 20.1 MB (10.67%) - LeetSync

--- 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.py
+++ 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.py
@@ -11,20 +11,25 @@ class Solution:
         def build_adj(node):
             if not node:
                 return
+
             if node.val not in adj:
                 adj[node.val] = []
+
             if node.left:
                 adj[node.val].append(node.left.val)
                 if node.left.val not in adj:
                     adj[node.left.val] = []
                 adj[node.left.val].append(node.val)
+
             if node.right:
                 adj[node.val].append(node.right.val)
                 if node.right.val not in adj:
                     adj[node.right.val] = []
                 adj[node.right.val].append(node.val)
+                
             build_adj(node.left)
             build_adj(node.right)
+
         build_adj(root)
  
         q =deque()
